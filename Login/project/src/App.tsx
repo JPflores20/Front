c2dbca71001b5da53afc0118039a2a7649c7e234
JPflores20 <pepe.jlfc.16@gmail.com>
2025-04-30 11:01:56 -0600

    Front Login

---
+++ Login/project/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginPage from './components/auth/LoginPage';
+
+function App() {
+  return (
+    <LoginPage />
+  );
+}
+
+export default App;
---
+++ Login/project/src/components/Page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface IPageProps {
+}
+
+const Page: React.FunctionComponent<IPageProps> = (props) => {
+  return <div>
+    El joto
+    
+  </div>;
+};
+
+export default Page;
